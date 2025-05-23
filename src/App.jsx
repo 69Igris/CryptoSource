2ed5920ad2a10a2284c14cec0eaf376bc0f78fb7
Aaryan Yadav <coder69.igris@gmail.com>
2025-05-23 12:58:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import { Link, Route , Routes } from 'react-router-dom'
+import Home from './components/pages/Home/Home.jsx'
+import Coin from './components/pages/Coin/Coin.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/coin/:coinId' element={<Coin/>}/>
+        <Route />
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/pages/Coin/Coin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Coin.css'
+import { Link } from 'react-router-dom'
+const Coin = () => {
+  return (
+    <div>Coin</div>
+  )
+}
+
+export default Coin
